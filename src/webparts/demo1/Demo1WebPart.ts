86cfae0b898f03fec99453697bd09aa093cfd93e
Rajesh <rajesh157@gmail.com>
2019-10-14 16:55:00 -0400

    update code

--- src/webparts/demo1/Demo1WebPart.ts
+++ src/webparts/demo1/Demo1WebPart.ts
@@ -13,13 +13,15 @@ import { escape } from '@microsoft/sp-lodash-subset';
 
 import styles from './Demo1WebPart.module.scss';
 import * as strings from 'Demo1WebPartStrings';
+import * as jquery from 'jquery';
+import { IDigestCache, DigestCache } from '@microsoft/sp-http';
 
 export interface IDemo1WebPartProps {
   description: string;
   comments: string;
   isSPFx: boolean;
   version: string;
-  isValid: boolean
+  isValid: boolean;
 }
 
 export interface ISPList{
@@ -29,6 +31,7 @@ export interface ISPList{
 export interface ISPLists{
   value: ISPList[];
 }
+
 
 export default class Demo1WebPart extends BaseClientSideWebPart<IDemo1WebPartProps> {
 
@@ -55,7 +58,7 @@ export default class Demo1WebPart extends BaseClientSideWebPart<IDemo1WebPartPro
         <li>${item.Title}</li>
       
       `;
-    })
+    });
 html+='</ul>';
   const listContainer: Element = this.domElement.querySelector('#dtReport');
   listContainer.innerHTML=html;
@@ -69,7 +72,7 @@ html+='</ul>';
   {
     this._getMockListData().then((response) =>{
       this._RenderData(response.value);
-    })
+    });
   }
   else if(Environment.type === EnvironmentType.SharePoint || Environment.type === EnvironmentType.ClassicSharePoint)
   {
@@ -77,7 +80,7 @@ html+='</ul>';
     this._getListData().then((response) =>{
       console.log(response.value);
       this._RenderData(response.value);
-    })
+    });
   }
 }
   public render(): void {
@@ -99,13 +102,148 @@ html+='</ul>';
               <a href="https://aka.ms/spfx" class="${ styles.button }">
                 <span class="${ styles.label }">Learn more</span>
               </a>
+              <button class="ms-Button read-httpButton ${styles.button} readall-Button">
+                <span class="ms-Button-label">Read httpClient All item</span>
+              </button>
+              <button class="ms-Button read-ajaxButton ${styles.button} readall-Button">
+                <span class="ms-Button-label">Read Ajax All item</span>
+              </button>
+              <button class="ms-Button create-httpButton ${styles.button} readall-Button">
+                <span class="ms-Button-label">Creare http item</span>
+              </button>
+              <button class="ms-Button create-ajaxButton ${styles.button} readall-Button">
+                <span class="ms-Button-label">Create Ajax item</span>
+              </button>
+              <div class="ms-Grid-row ms-bgColor-themeDark ms-fontColor-white ${styles.row}">
+                <div class="ms-Grid-col ms-u-lg10 ms-u-xl8 ms-u-xlPush2 ms-u-lgPush1">
+                  <div class="status"></div>
+                  <ul class="items"><ul>
+                </div>
+              </div>
+              
             </div>
+            
           </div>
         </div>
         <div id="dtReport"></div>
       </div>`;
-     this._renderListAsync();
+      const webPart: Demo1WebPart = this;
+  this.domElement.querySelector('button.read-httpButton').addEventListener('click', () => { webPart.readHttpItem(); });
+  this.domElement.querySelector('button.read-ajaxButton').addEventListener('click', () => { webPart.readAjaxItem(); });
+  this.domElement.querySelector('button.create-httpButton').addEventListener('click', () => { webPart.createHttpItem(); });
+  this.domElement.querySelector('button.create-ajaxButton').addEventListener('click', () => { webPart.createAjaxItem(); });
+
+     //this._renderListAsync();
   }
+
+  //create ajax item
+  private createAjaxItem(): void {
+    alert("ajax");
+    const digestCache: IDigestCache = this.context.serviceScope.consume(DigestCache.serviceKey);
+
+  digestCache.fetchDigest(this.context.pageContext.web.serverRelativeUrl)
+    .then((digest: string) => {
+
+    jquery.ajax({    
+      url: `${this.context.pageContext.web.absoluteUrl}/_api/web/lists/getbytitle('Employees')/items`,    
+      type: "POST",    
+      headers:{
+        "accept": "application/json;odata=verbose",  
+        "content-type": "application/json;odata=verbose",
+        "X-RequestDigest": digest
+      },
+      data: JSON.stringify({  
+        '__metadata': {  
+            'type': 'SP.Data.EmployeesListItem'  
+        },  
+        'Title': 'Name1'
+    }),  
+      success: (resultData) => {             
+          //alert(resultData.d.results);
+          this.updateStatus(`Item successfully created`);
+      },    
+      error : (errorThrown) => {  
+        this.updateStatus('Loading all items failed with error: ' + JSON.stringify(errorThrown));
+      }    
+  });
+
+
+});
+  }
+  //create http item
+  private createHttpItem(): void {
+    const body: string = JSON.stringify({  
+      'Title': `Item ${new Date()}`  
+    });  
+    
+    this.context.spHttpClient.post(`${this.context.pageContext.web.absoluteUrl}/_api/web/lists/getbytitle('Employees')/items`,  
+    SPHttpClient.configurations.v1,  
+    {  
+      headers: {  
+        'Accept': 'application/json;odata=nometadata',  
+        'Content-type': 'application/json;odata=nometadata',  
+        'odata-version': ''  
+      },  
+      body: body  
+    })  
+    .then((response: SPHttpClientResponse): Promise<ISPList> => {  
+      return response.json();  
+    })  
+    .then((item: ISPList): void => {       
+      this.updateStatus(`Item '${item.Title}' (ID: ${item.Id}) successfully created`);
+    }, (error: any): void => {  
+      this.updateStatus('Loading all items failed with error: ' + error);
+    });  
+  }
+
+
+  //read ietms by ajax
+  private readAjaxItem(): void {
+    var reactHandler = this;    
+    jquery.ajax({    
+        url: `${this.context.pageContext.web.absoluteUrl}/_api/web/lists/getbytitle('Employees')/items?$select=Title,Id`,    
+        type: "GET",    
+        headers:{'Accept': 'application/json; odata=verbose;'},    
+        success: (resultData) => {             
+            //alert(resultData.d.results);
+            this.updateStatus(`Successfully loaded ${resultData.d.results.length} items`, resultData.d.results);
+        },    
+        error : (jqXHR, textStatus, errorThrown) => {  
+          this.updateStatus('Loading all items failed with error: ' + errorThrown);
+        }    
+    });    
+  }
+
+  //Read Item by spHttpClient
+  private readHttpItem(): void {
+    this.updateStatus('Loading all items...');
+    this.context.spHttpClient.get(`${this.context.pageContext.web.absoluteUrl}/_api/web/lists/getbytitle('Employees')/items?$select=Title,Id`,
+      SPHttpClient.configurations.v1,
+      {
+        headers: {
+          'Accept': 'application/json;odata=nometadata',
+          'odata-version': ''
+        }
+      })
+      .then((response: SPHttpClientResponse): Promise<{ value: ISPList[] }> => {
+        return response.json();
+      })
+      .then((response: { value: ISPList[] }): void => {
+        this.updateStatus(`Successfully loaded ${response.value.length} items`, response.value);
+      }, (error: any): void => {
+        this.updateStatus('Loading all items failed with error: ' + error);
+      });
+  }
+
+  private updateStatus(status: string, items: ISPList[] = []): void {
+    this.domElement.querySelector('.status').innerHTML = status;
+    this.updateItemsHtml(items);
+  }
+
+  private updateItemsHtml(items: ISPList[]): void {
+    this.domElement.querySelector('.items').innerHTML = items.map(item => `<li>${item.Title} (${item.Id})</li>`).join("");
+  }
+  //
 
   protected get dataVersion(): Version {
     return Version.parse('1.0');
